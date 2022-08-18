aa86770ac7c8e71ffe9f8643b8a59013c49eba1e
Victor <victor.ti.souza@gmail.com>
2022-08-18 14:03:11 -0300

    use environment variables

--- db.go
+++ db.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"database/sql"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"os"
 )
 
 func Conectar() *sql.DB {
+	godotenv.Load(".env")
+
 	host := os.Getenv("HOST")
 	port := os.Getenv("PORT")
 	user := os.Getenv("DB_USER")
 	pass := os.Getenv("DB_PASS")
 	dbName := os.Getenv("DB_NAME")
 
-	db, err := sql.Open("postgres", "postgres://"+user+":"+pass+"@"+host+":"+port+"/"+dbName)
+	connStr := "postgres://" + user + ":" + pass + "@" + host + ":" + port + "/" + dbName
+
+	db, err := sql.Open("postgres", connStr)
 
 	if err != nil {
 		panic(err.Error())
