4472fc429c5e4e5938d311d25f11911a58589fcf
Victor <victor.ti.souza@gmail.com>
2022-08-18 13:53:43 -0300

    use environment variables

--- db.go
+++ db.go
@@ -3,10 +3,17 @@ package main
 import (
 	"database/sql"
 	_ "github.com/lib/pq"
+	"os"
 )
 
 func Conectar() *sql.DB {
-	db, err := sql.Open("postgres", "postgres://postgres:api-test-pass@id-instancia-rds-teste.c0islweyishx.us-east-1.rds.amazonaws.com:5432/api_test_db")
+	host := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	dbName := os.Getenv("DB_NAME")
+
+	db, err := sql.Open("postgres", "postgres://"+user+":"+pass+"@"+host+":"+port+"/"+dbName)
 
 	if err != nil {
 		panic(err.Error())
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module API-Teste
 
 go 1.18
 
-require github.com/lib/pq v1.10.6 // indirect
+require (
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/lib/pq v1.10.6 // indirect
+)
